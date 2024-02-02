94a1d40843e349d8665d971986141620fe06dab7
Johnathan James <haxwell@gmail.com>
2024-02-01 18:49:45 -0700

    Added impl to return a string of a given length

--- src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
@@ -3,6 +3,12 @@ package com.savvato.tribeapp.services;
 public class TestDrivenDevelopmentExampleServiceImpl implements TestDrivenDevelopmentExampleService {
     @Override
     public String getString(int length) {
-        return "abc";
+        String rtn = "";
+
+        for (int i= 0; i < length; i++) {
+            rtn += "a";
+        }
+
+        return rtn;
     }
 }
