39ef79cac8898e3a6d06dc5ea3a7178cc3b33119
Johnathan James <haxwell@gmail.com>
2024-02-01 19:29:29 -0700

    Added parameter error checking

--- src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
@@ -5,6 +5,19 @@ import java.util.ArrayList;
 public class TestDrivenDevelopmentExampleServiceImpl implements TestDrivenDevelopmentExampleService {
     @Override
     public String[] getString(int length, char ch, int arrayLength) {
+
+        if (length < 1 || length > 100) {
+            throw new IllegalArgumentException("Length must be between 1 and 100");
+        }
+
+        if (ch < 'A' || ch > 'z') {
+            throw new IllegalArgumentException("Character must be between a and z");
+        }
+
+        if (arrayLength < 1 || arrayLength > 100) {
+            throw new IllegalArgumentException("Array length must be between 1 and 100");
+        }
+
         String rtn;
         int index = 1;
 
