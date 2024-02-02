8564db70cdb304dfad0970e44409152baee4ada5
Johnathan James <haxwell@gmail.com>
2024-02-01 18:42:43 -0700

    Added initial implementation, return abc

--- src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/TestDrivenDevelopmentExampleServiceImpl.java
@@ -3,6 +3,6 @@ package com.savvato.tribeapp.services;
 public class TestDrivenDevelopmentExampleServiceImpl implements TestDrivenDevelopmentExampleService {
     @Override
     public String getString() {
-        return null;
+        return "abc";
     }
 }
