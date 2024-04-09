92f0216545bda40468c2da6cff38817d95bcc86d
maryamkhan14 <kmaryam818@gmail.com>
2024-04-09 16:51:25 -0700

    Update SMSChallengeCodeAPIController endpoint names

--- src/main/java/com/savvato/tribeapp/controllers/SMSChallengeCodeAPIController.java
+++ src/main/java/com/savvato/tribeapp/controllers/SMSChallengeCodeAPIController.java
@@ -15,47 +15,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/public")
+@RequestMapping("/api/public/sms-challenge-code")
 @Tag(name = "public")
 @Slf4j
 public class SMSChallengeCodeAPIController {
-  @Autowired SMSChallengeCodeService smsccs;
+    @Autowired
+    SMSChallengeCodeService smsccs;
 
-  @SendSMSChallengeCode
-  @PostMapping("/sendSMSChallengeCodeToPhoneNumber")
-  public String sendSMSChallengeCode(@RequestBody @Valid SMSChallengeRequest req) {
-    String phoneNumber =
-        req.phoneNumber; // assume the number we're getting is 10 digits, without the country code
+    @SendSMSChallengeCode
+    @PostMapping
+    public String sendSMSChallengeCode(@RequestBody @Valid SMSChallengeRequest req) {
+        String phoneNumber =
+                req.phoneNumber; // assume the number we're getting is 10 digits, without the country code
 
-    if (!phoneNumber.startsWith("0")) phoneNumber = "1" + phoneNumber;
+        if (!phoneNumber.startsWith("0")) phoneNumber = "1" + phoneNumber;
 
-    String rtn = smsccs.sendSMSChallengeCodeToPhoneNumber(phoneNumber);
-    log.debug("Sent challenge code to " + phoneNumber + ". " + rtn);
-    return rtn;
-  }
-
-  @ClearSMSChallengeCode
-  @PostMapping("/clearSMSChallengeCodeToPhoneNumber")
-  public String clearSMSChallengeCode(HttpServletRequest request, Model model) {
-    String phoneNumber =
-        "1"
-            + request.getParameter(
-                "phoneNumber"); // assume the number we're getting is 10 digits, without the country
-                                // code
-    smsccs.clearSMSChallengeCodeToPhoneNumber(phoneNumber);
-    return "ok";
-  }
-
-  @IsAValidSMSChallengeCode
-  @PostMapping("/isAValidSMSChallengeCode")
-  public boolean isAValidSMSChallengeCode(@RequestBody @Valid SMSChallengeRequest req) {
-
-    if ((req.phoneNumber == null || req.phoneNumber.equals("null"))
-        || (req.code == null || req.code.equals("null"))) {
-      throw new IllegalArgumentException(
-          "Cannot check for valid SMS challenge code with null phoneNumber or challenge code.");
+        String rtn = smsccs.sendSMSChallengeCodeToPhoneNumber(phoneNumber);
+        log.debug("Sent challenge code to " + phoneNumber + ". " + rtn);
+        return rtn;
     }
 
-    return smsccs.isAValidSMSChallengeCode(req.phoneNumber, req.code);
-  }
+    @ClearSMSChallengeCode
+    @DeleteMapping
+    public String clearSMSChallengeCode(HttpServletRequest request, Model model) {
+        String phoneNumber =
+                "1"
+                        + request.getParameter(
+                        "phoneNumber"); // assume the number we're getting is 10 digits, without the country
+        // code
+        smsccs.clearSMSChallengeCodeToPhoneNumber(phoneNumber);
+        return "ok";
+    }
+
+    @IsAValidSMSChallengeCode
+    @PostMapping("/validity-status")
+    public boolean isAValidSMSChallengeCode(@RequestBody @Valid SMSChallengeRequest req) {
+
+        if ((req.phoneNumber == null || req.phoneNumber.equals("null"))
+                || (req.code == null || req.code.equals("null"))) {
+            throw new IllegalArgumentException(
+                    "Cannot check for valid SMS challenge code with null phoneNumber or challenge code.");
+        }
+
+        return smsccs.isAValidSMSChallengeCode(req.phoneNumber, req.code);
+    }
 }
