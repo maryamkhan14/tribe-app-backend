41dab9fab389c2fa35873ffb5df009143e45839e
maryamkhan14 <kmaryam818@gmail.com>
2024-04-09 16:52:06 -0700

    Update UserAPIController endpoint names

--- src/main/java/com/savvato/tribeapp/controllers/UserAPIController.java
+++ src/main/java/com/savvato/tribeapp/controllers/UserAPIController.java
@@ -12,101 +12,105 @@ import com.savvato.tribeapp.services.SMSChallengeCodeService;
 import com.savvato.tribeapp.services.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
-import java.util.Optional;
-import javax.validation.Valid;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/api/public/user")
+@RequestMapping("/api/public/users")
 @Tag(
-    name = "public/user",
-    description =
-        "Publicly available user-information relevant paths, no login/credentials needed to make requests to them")
+        name = "public/users",
+        description =
+                "Publicly available user-information relevant paths, no login/credentials needed to make requests to them")
 public class UserAPIController {
 
-  @Autowired UserService userService;
+    @Autowired
+    UserService userService;
 
-  @Autowired ProfileService profileService;
+    @Autowired
+    ProfileService profileService;
 
-  @Autowired UserRepository ur;
+    @Autowired
+    UserRepository ur;
 
-  @Autowired SMSChallengeCodeService smsccs;
+    @Autowired
+    SMSChallengeCodeService smsccs;
 
-  UserAPIController() {}
-
-  @CreateNewUser
-  @PostMapping("/new")
-  public ResponseEntity createUser(@RequestBody @Valid UserRequest req) {
-
-    try {
-      Optional<User> response = userService.createNewUser(req, Constants.SMS);
-
-      return new ResponseEntity<>(response.get(), HttpStatus.OK);
-    } catch (IllegalArgumentException iae) {
-      return new ResponseEntity(iae.getMessage(), HttpStatus.BAD_REQUEST);
+    UserAPIController() {
     }
-  }
 
-  // TODO: figure a way to have these enddpoints not be publicly accessible, but also not require a
-  // user to be logged in.
-  // 			a secret, but general purpose for-the-app login.
+    @CreateNewUser
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody @Valid UserRequest req) {
 
-  // api/public/user/isUsernameAvailable?q=sample
-  @IsUsernameAvailable
-  @GetMapping("/isUsernameAvailable")
-  public boolean isUsernameAvailable(
-      @Parameter(description = "The username", example = "admin") @RequestParam("q")
-          String queryStr) {
-    return this.ur.findByName(queryStr).isPresent() == false;
-  }
+        try {
+            Optional<User> response = userService.createNewUser(req, Constants.SMS);
 
-  // api/public/user/isPhoneNumberAvailable?q=7205870001
-  @IsPhoneNumberAvailable
-  @GetMapping("/isPhoneNumberAvailable")
-  public boolean isPhoneNumberAvailable(
-      @Parameter(description = "The phone number", example = "1234567890") @RequestParam("q")
-          String queryStr) {
-    Optional<List<User>> opt = this.ur.findByPhone(queryStr);
+            return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        } catch (IllegalArgumentException iae) {
+            return new ResponseEntity(iae.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
-    if (opt.isPresent()) return opt.get().size() == 0;
-    else return true;
-  }
+    // TODO: figure a way to have these enddpoints not be publicly accessible, but also not require a
+    // user to be logged in.
+    // 			a secret, but general purpose for-the-app login.
 
-  // api/public/user/isEmailAddressAvailable?q=anAddress@domain.com
-  @IsEmailAddressAvailable
-  @GetMapping("/isEmailAddressAvailable")
-  public boolean isEmailAddressAvailable(
-      @Parameter(description = "The email address", example = "admin@tribeapp.com")
-          @RequestParam("q")
-          String queryStr) {
-    return this.ur.findByEmail(queryStr).isPresent() == false;
-  }
 
-  // api/public/user/isUserInformationUnique?name=sample&phone=7205870001&email=anAddress@domain.com
-  @IsUserInformationUnique
-  @GetMapping("/isUserInformationUnique")
-  public String isUserInformationUnique(
-      @Parameter(description = "The username", example = "admin") @RequestParam("name")
-          String username,
-      @Parameter(description = "The phone number", example = "1234567890") @RequestParam("phone")
-          String phone,
-      @Parameter(description = "The email address", example = "admin@tribeapp.com")
-          @RequestParam("email")
-          String email) {
-    if (!isUsernameAvailable(username)) return "{\"response\": \"username\"}";
-    if (!isPhoneNumberAvailable(phone)) return "{\"response\": \"phone\"}";
-    if (!isEmailAddressAvailable(email)) return "{\"response\": \"email\"}";
+    private boolean isUsernameAvailable(
+            @Parameter(description = "The username", example = "admin") @RequestParam("q")
+            String queryStr) {
+        return !this.ur.findByName(queryStr).isPresent();
+    }
 
-    return "{\"response\": true}";
-  }
+    // api/public/user/isPhoneNumberAvailable?q=7205870001
+    private boolean isPhoneNumberAvailable(
+            @Parameter(description = "The phone number", example = "1234567890") @RequestParam("q")
+            String queryStr) {
+        Optional<List<User>> opt = this.ur.findByPhone(queryStr);
 
-  @ChangePassword
-  @PostMapping("/changePassword")
-  public UserDTO changePassword(@RequestBody @Valid ChangePasswordRequest request) {
-    return userService.changePassword(request.pw, request.phoneNumber, request.smsChallengeCode);
-  }
+        if (opt.isPresent()) return opt.get().size() == 0;
+        else return true;
+    }
+
+    // api/public/user/isEmailAddressAvailable?q=anAddress@domain.com
+    private boolean isEmailAddressAvailable(
+            @Parameter(description = "The email address", example = "admin@tribeapp.com")
+            @RequestParam("q")
+            String queryStr) {
+        return !this.ur.findByEmail(queryStr).isPresent();
+    }
+
+    // api/public/user/isUserInformationUnique?name=sample&phone=7205870001&email=anAddress@domain.com
+    @IsUserInformationUnique
+    @GetMapping("/availability")
+    public ResponseEntity<String> isUserInformationUnique(
+            @Parameter(description = "The username", example = "admin") @RequestParam(name = "username", required = false)
+            String username,
+            @Parameter(description = "The phone number", example = "1234567890") @RequestParam(name = "phone", required = false)
+            String phone,
+            @Parameter(description = "The email address", example = "admin@tribeapp.com")
+            @RequestParam(name = "email", required = false)
+            String email) {
+        if (StringUtils.isBlank(username) && StringUtils.isBlank(email) && StringUtils.isBlank(phone)) {
+            return ResponseEntity.badRequest().body("Please provide at least one parameter");
+        }
+        if (!isUsernameAvailable(username)) return ResponseEntity.ok("{\"response\": \"username\"}");
+        if (!isPhoneNumberAvailable(phone)) return ResponseEntity.ok("{\"response\": \"phone\"}");
+        if (!isEmailAddressAvailable(email)) return ResponseEntity.ok("{\"response\": \"email\"}");
+
+        return ResponseEntity.ok("{\"response\": true}");
+    }
+
+    @ChangePassword
+    @PostMapping("/password-reset")
+    public UserDTO changePassword(@RequestBody @Valid ChangePasswordRequest request) {
+        return userService.changePassword(request.pw, request.phoneNumber, request.smsChallengeCode);
+    }
 }
