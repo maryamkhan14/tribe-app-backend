b867107e9dd449d1985387f0c896eb13398d3a6e
maryamkhan14 <kmaryam818@gmail.com>
2024-04-30 16:35:35 -0700

    Update public endpoint names in SecurityConfig

--- src/main/java/com/savvato/tribeapp/config/SecurityConfig.java
+++ src/main/java/com/savvato/tribeapp/config/SecurityConfig.java
@@ -1,9 +1,5 @@
 package com.savvato.tribeapp.config;
 
-import java.util.Arrays;
-
-import javax.servlet.http.HttpServletResponse;
-
 import com.savvato.tribeapp.config.filters.JwtTokenFilter;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
@@ -25,6 +21,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @SecurityScheme(
@@ -36,7 +36,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
-    
+
     private final JwtTokenFilter jwtTokenFilter;
 
     @Autowired
@@ -54,39 +54,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         // Enable CORS and disable CSRF
-    	http = http.cors().and().csrf().disable();
-    	
-    	// Set session management to stateless
-    	http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
-    	
-    	// Set unauthorized requests exception handler
-    	http = http.exceptionHandling()
-    			.authenticationEntryPoint(
-    				(request, response, ex) -> {
-    					response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
-    				}
-    			).and();
-    	// Set permissions on endpoints
-    	http.authorizeRequests()
-        .antMatchers(HttpMethod.HEAD, "/api/resource/topic/*").permitAll()  // this is because of the saveAs extension we use on the front end.. it won't let us pass in headers, so no authorization can be had.. ASAP, find a better way.
-        .antMatchers(HttpMethod.GET, "/api/resource/topic/*").permitAll()  // this is because of the saveAs extension we use on the front end.. it won't let us pass in headers, so no authorization can be had.. ASAP, find a better way.
-    	.antMatchers(HttpMethod.POST, "/api/public/login").permitAll()
-    	.antMatchers(HttpMethod.POST, "/api/public/user/new").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/public/user/isEmailAddressAvailable").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/public/user/isPhoneNumberAvailable").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/public/user/isUsernameAvailable").permitAll()
-        .antMatchers(HttpMethod.POST, "/api/public/user/changeLostPassword").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/public/user/isUserInformationUnique").permitAll()
-        .antMatchers(HttpMethod.POST, "/api/public/sendSMSChallengeCodeToPhoneNumber").permitAll()
-        .antMatchers(HttpMethod.POST, "/api/public/isAValidSMSChallengeCode").permitAll()
-        .antMatchers("/swagger-ui/**", "/swagger-ui**", "/docs/**", "/docs**").permitAll()
-    	.anyRequest().hasAnyRole("admin,accountholder");
+        http = http.cors().and().csrf().disable();
 
-    	
-    	// Add JWT token filter
-    	http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        // Set session management to stateless
+        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+
+        // Set unauthorized requests exception handler
+        http = http.exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                        }
+                ).and();
+        // Set permissions on endpoints
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.HEAD, "/api/files/topic/*").permitAll()  // this is because of the saveAs extension we use on the front end.. it won't let us pass in headers, so no authorization can be had.. ASAP, find a better way.
+                .antMatchers(HttpMethod.GET, "/api/files/topic/*").permitAll()  // this is because of the saveAs extension we use on the front end.. it won't let us pass in headers, so no authorization can be had.. ASAP, find a better way.
+                .antMatchers(HttpMethod.POST, "/api/public/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/public/users").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/public/users/availability").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/public/users/password-reset").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/public/sms-challenge-code").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/public/sms-challenge-code/validity-status").permitAll()
+                .antMatchers("/swagger-ui/**", "/swagger-ui**", "/docs/**", "/docs**").permitAll()
+                .anyRequest().hasAnyRole("admin,accountholder");
+
+
+        // Add JWT token filter
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
-    
+
     //====================encoders
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -97,16 +94,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD","GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    @Override @Bean
+    @Override
+    @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
