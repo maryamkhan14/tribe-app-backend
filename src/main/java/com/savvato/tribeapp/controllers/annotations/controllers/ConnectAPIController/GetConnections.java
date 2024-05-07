e9d0e618d8e96f51a4db7b8382cfd81cae6a6de5
maryamkhan14 <kmaryam818@gmail.com>
2024-05-07 16:29:56 -0700

    Update GetConnections Swagger documentation

--- src/main/java/com/savvato/tribeapp/controllers/annotations/controllers/ConnectAPIController/GetConnections.java
+++ src/main/java/com/savvato/tribeapp/controllers/annotations/controllers/ConnectAPIController/GetConnections.java
@@ -2,16 +2,23 @@ package com.savvato.tribeapp.controllers.annotations.controllers.ConnectAPIContr
 
 import com.savvato.tribeapp.controllers.annotations.responses.BadRequest;
 import com.savvato.tribeapp.controllers.annotations.responses.Success;
+import com.savvato.tribeapp.dto.ConnectOutgoingMessageDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.lang.annotation.*;
 
-/** Documentation for getting a QR code string */
+/**
+ * Documentation for getting a QR code string
+ */
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Operation(
-    summary = "Get connections for a user",
-    description = "Provided a user ID, get Connections for a user. ")
-@Success(description = "", example = "")
-@BadRequest(description = "", noContent = true)
-public @interface GetConnections{}
+        summary = "Get connections for a user",
+        description = "Provided a user ID, get Connections for a user. ")
+@Success(description = "A list of the user's connection", array = @ArraySchema(schema = @Schema(implementation = ConnectOutgoingMessageDTO.class), minItems = 2))
+@BadRequest(description = "Connections could not be retrieved", noContent = true)
+public @interface GetConnections {
+}
